85a0f2481150a55a66b5235e12e360e38d7e4832
Sergey Sorokin <sss.sss.sorokin@yandex.ru>
2022-09-15 18:55:22 +0500

    зелёная форма и нет кнопки закрыть

--- MyNotepad/Form1.Designer.cs
+++ MyNotepad/Form1.Designer.cs
@@ -29,26 +29,14 @@ namespace MyNotepad
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(111, 87);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 50);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Закрыть";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(1011, 312);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -57,8 +45,6 @@ namespace MyNotepad
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
